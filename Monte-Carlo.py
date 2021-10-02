bd005f214969e04a7a0315e714d9bff219ce93da
Martin Vérot <martin.verot@ens-lyon.fr>
2021-10-02 09:31:40 +0200

    amélioration des commentaires pour faciliter l utilisation du script

--- Monte-Carlo.py
+++ Monte-Carlo.py
@@ -7,6 +7,36 @@ import numpy.ma as ma
 from scipy.stats import norm
 import scipy.stats as stats
 
+"""
+Script écrit par Martin Vérot, PRAG en chimie à l'ENS de Lyon, grâce à un script original de Jean Lamerenx
+https://jlamerenx.fr/python/
+
+Il est possible de changer le nombre de tirages simulés, les paramètres d'entrée et la fonction.
+
+Pour définir un paramètre d'entrée, il faut lui donner un nom, une forme de distribution, une valeur la plus probable et une étendue. Les types de distribution possibles sont :
+    - 'triangulaire', dans ce cas, l'étendue est la demi-largeur de la distribution 
+    - 'rectangulaire', dans ce cas, l'étendue est la demi-largeur de la distribution 
+    - 'gaussienne', dans ce cas, l'étendue est l'écart-type de la distribution
+    - pour rajouter de nouvelles formes de distribution, il faut modifier la fonction "tirage" en consequence
+
+Ensuite, il faut définir la fonction qui calcule la variable de sortie en fonction de l'ensemble des variables d'entrée : z=f(x_1,...x_n)
+La fonction est définie à l'avant dernière ligne de la fonction "grandeurExtraite" où il faut remplacer chaque variable x_i par valeurs('x_i',params,value)
+
+Par exemple, pour calculer z=B*exp(-k*E) avec B, k, et E les paramètres d'entrée, il faudrait changer pour avoir quelque chose similaire à ;
+Adaptation de la définition des paramètres d'entrée à notre cas :
+####################### 
+params = [
+    {'name':'B','distribution':'triangulaire','valeur':0.1,'etendue':0.01},
+    {'name':'k','distribution':'rectangulaire','valeur':10.0,'etendue':1.0},
+    {'name':'E','distribution':'gaussienne','valeur':10.0,'etendue':0.1}
+]
+#######################
+Changement de la fonction à calculer pour le paramètre de sortie :
+#######################
+    OutputValue = valeurs('B',params,value)*np.exp(valeurs('k',params,value)*valeurs('E',params,value))
+#######################
+"""
+
 #Nombre de tirages simulés
 N=100000                                                                         
 #paramètres utilisés et leur distribution
@@ -32,6 +62,11 @@ def grandeurExtraite(params,N):
     ##################
     OutputValue = valeurs('Cb',params,value)*valeurs('Vb',params,value)/valeurs('Va',params,value)
     return (OutputValue,value)
+
+
+############################################
+##Il n'y a plus rien à changer au-delà de cette ligne
+############################################
 
 def tirage(param,N):
     """
@@ -82,13 +117,10 @@ Output = np.sort(Output)
 moy,u = PropOutput(Output)
 
 
-Ccla=10
-uCla=np.sqrt((0.1/(10*np.sqrt(3)))**2+(0.001/(np.sqrt(3)*0.1))+(0.001/(np.sqrt(6)*0.1)))
 
 #abscisse
 x = np.linspace(moy-5*u,moy+5*u,100)
 Gauss = stats.norm.pdf(x, moy, u)
-#GaussCla = stats.norm.pdf(x, Ccla, uCla)
 nbParams = len(params)
 SquareGridSize=int(np.ceil(np.sqrt(nbParams)))
 widths = np.ones(SquareGridSize+1)
@@ -101,20 +133,19 @@ gs = fig.add_gridspec(SquareGridSize,SquareGridSize+1,  width_ratios=tuple(width
 ax1 = fig.add_subplot(gs[:int(np.ceil(SquareGridSize/2)),SquareGridSize])
 ax1.hist(Output, bins='auto',density = True, label = "Monte-Carlo")
 ax1.plot(x, Gauss, 'k-', label = "Gaussienne associée")
-#plt.plot(x, GaussCla, 'k-',color='C1', label = "Gaussienne associée")
 fig.suptitle('Simulation de {} titrages Ca = {:.3e} mol/L u(Ca) = {:3e}'.format(N,moy,u))
-ax1.set_xlabel('Ca en mol/L$')
+ax1.set_xlabel('Ca en mol/L')
 ax1.legend()
 
+#Tracé de l'intervalle de confiance lié à la largeur de l'intervalle
 ax2 = fig.add_subplot(gs[int(np.ceil(SquareGridSize/2)):,SquareGridSize])
 Intervalles = []
 x2 = np.linspace(0,5*u,100)
 for value in x2.tolist():
     Intervalles.append(IntervalleConfiance(value, Output,moy))
 ax2.plot(x2, Intervalles, 'k-', label = "")
-#plt.plot(x, GaussCla, 'k-',color='C1', label = "Gaussienne associée")
-ax2.set_xlabel('largeur de l\'intervalle')
-ax2.set_ylabel('Interval de confiance')
+ax2.set_xlabel('largeur de l\'intervalle $U$ ($z=z_0\pm U$)')
+ax2.set_ylabel('Intervalle de confiance')
 
 
 #Plot de chacune des valeurs d'entrée
@@ -126,9 +157,6 @@ for i in range(nbParams):
     ax = fig.add_subplot(gs[ligne,colonne])
     ax.hist(values[i],bins='auto',density = True, label = params[i]['name'])
     ax.legend(loc='upper right')
-
-#ax3 = fig.add_subplot(gs[1,1])
-
 
 plt.show()
 
